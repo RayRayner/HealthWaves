a187e0cfd2c34872bb1dcf8465379b227a7736c3
Rayner <robrayner96@hotmail.com>
2019-04-18 00:35:24 +0100

    Updates to the health system stuff, IT WORKS! Now to add more functionality to make it a game

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int waveHealth;
+    public GameObject[] objs;
+    public Vector3 spawnLocation;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("spawnPeople");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	private IEnumerator spawnPeople () {
+        int spawned = Mathf.FloorToInt(Random.Range(1, 5));
+        Instantiate(objs[spawned-1], spawnLocation, Quaternion.identity);
+        yield return new WaitForSeconds(0.1f);
+        waveHealth -= spawned;
+        spawnLocation += (Vector3.forward*2);
+        if (waveHealth > 0) {
+			StartCoroutine("spawnPeople");
+		}
+
+    }
 }
